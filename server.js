a5b9f307ed33373543f513e1fa70905b1d92699c
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-16 15:07:34 -0400

    removed files from develop and now it displays web page but it doesnt GET the notes

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-// // Import required modules
-// const express = require('express');
-// const html_routes = require('./routes/html-routes')
-// const api_routes = require('./routes/ApiRoutes/api-routes')
-
-// // Set the port dynamically or default to 3000
-// const PORT = process.env.PORT || 3001;
-
-// // Create an instance of Express
-// const app = express();
-
-// //Middeware setup
-// //Parse URL-encoded and JSON data
-// app.use(express.urlencoded({ extended : true }));
-// app.use(express.json());
-
-// // Serve static files from the "public" directory
-// app.use(express.static("public"));
-
-// // Use HTML routes defined in html-routes file
-// app.use(html_routes)
-
-// // Use API routes defined in api-routes file
-// app.use(api_routes)
-
-// //Start the server and listen on the specified port
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const html_routes = require('./routes/html-routes')
