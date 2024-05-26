8d5a217045084039b2a68347b46a3b1acc7d54dc
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-05-26 01:17:32 -0400

    did some work for the console to load with no errors

--- server.js
+++ server.js
@@ -20,23 +20,57 @@
 // app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
 
+// const express = require('express');
+// const path = require('path');
+// const html_routes = require('./routes/html-routes')
+// const api_routes = require('./routes/api-routes')
+// const PORT = process.env.PORT || 3001;
+// // dynamically set the port
+// const app = express();
+
+// // Express middleware will always run the operation in the order from top to bottom "order matters"
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// const publicPath = 'C:/Users/Edwin/bootcamp/homework/Note-Taker/';
+// app.use(express.static(publicPath));
+
+// // app.use(express.static(path.join(__dirname, 'public')));
+// app.use(html_routes)
+// app.use(api_routes)
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+
 const express = require('express');
 const path = require('path');
-const html_routes = require('./routes/html-routes')
-const api_routes = require('./routes/api-routes')
+const html_routes = require('./routes/html-routes');
+const api_routes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
-// dynamically set the port
+
 const app = express();
 
-// Express middleware will always run the operation in the order from top to bottom "order matters"
+// Middleware to parse request bodies and serve static files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const publicPath = 'C:/Users/Edwin/bootcamp/homework/Note-Taker/public';
-app.use(express.static(publicPath));
 
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(html_routes)
-app.use(api_routes)
+// Serve static files from the 'assets' directory
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Serve index.html from the root directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve notes.html from the root directory
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+// Use routes
+app.use(html_routes);
+app.use(api_routes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
